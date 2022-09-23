6abf034d205c8a92d31dfcb8e602d5504273f9d8
leonardo <leonardommascarenhas@gmail.com>
2022-09-23 16:16:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+  buildTree(arr) {
+    const sortedArray = arr.sort((a, b) => a - b);
+    const sortedArrayToBST = (mid) => {};
+  }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
