4ad782839b7f4efb3fe2e9a7c0d28c57ac53656e
leonardo <leonardommascarenhas@gmail.com>
2022-09-23 17:52:57 -0300

    commit

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ class Tree {
   constructor() {
     this.root = null;
   }
-  buildTree(arr) {
-    const sortedArray = arr.sort((a, b) => a - b);
-    const sortedArrayToBST = (mid) => {};
-  }
 }
+
+const sortedArrayToBST = (arr, start, end) => {
+  if (start > end) {
+    return null;
+  }
+  let mid = parseInt((start + end) / 2);
+  let node = new Node(arr[mid]);
+  node.left = sortedArrayToBST(arr, start, mid - 1);
+  node.right = sortedArrayToBST(arr, mid + 1, end);
+  return node;
+};
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const sortedArray = [...new Set(array.sort((a, b) => a - b))];
+
+let n = sortedArray.length;
+root = sortedArrayToBST(sortedArray, 0, n - 1);
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node.right !== null) {
@@ -25,3 +38,5 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
 };
+console.log(sortedArray);
+prettyPrint(root);
